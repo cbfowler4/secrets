0e9b2d656ef4f113cc9b426fc140913af2767dda
Bryan <cbfowler4@gmail.com>
2018-02-22 17:02:18 -0500

    finished comments

--- secret.js
+++ secret.js
@@ -29,26 +29,36 @@ class Secret {
     return this;
   }
 
+  // sets this.handleError. handleError is called in the onReject function
   catch(handleError){
     this.handleError = handleError;
-
+    //returns this so that another then or catch may be called
     return this;
   }
 
+  //called in the secret on success
   onResolve(value){
+    //initiates value with given response from result
     let storedValue = value;
 
     try {
+      //runs through secrets that have been chained after
+      //are these all added to original instance synchronously?
       this.secretsChain.forEach((nextFunc) => {
+        //set next value equal to the result of calling current then func
+        //with the current value
         storedValue = nextFunc(storedValue);
       });
     } catch (error) {
+      //on failure, reset secrets chain because all downstream secrets will fail
       this.secretsChain = [];
-
+      //use error handler defined in constructor
       this.onReject(error);
     }
   }
 
+  // onReject only calls handle error as defined in the catch function chained
+  //on the original secret
   onReject(error) {
     this.handleError(error);
   }
