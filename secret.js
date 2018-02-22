3f577a65db5a8568b641db21713e84f14745fa1b
Bryan <cbfowler4@gmail.com>
2018-02-22 15:55:48 -0500

    basic secret class finished, comments being added

--- secret.js
+++ secret.js
@@ -1,17 +1,31 @@
 class Secret {
   constructor(executionFunction) {
+    //instantiate secret chain, will hold all of the functions,
+    // when .then is called secret will push function onto the secret chain
     this.secretsChain = [];
+
+    //when user calls catch, the function called will be assigned to
+    // handleError to process the error
     this.handleError = () => {};
 
     this.onResolve = this.onResolve.bind(this);
     this.onReject = this.onReject.bind(this);
 
+    /* secret is called and given onResolve and onReject as inputs
+      new Secret((resolve,reject)=> { if response is success {
+        resolve(results);
+      } else {
+        reject(errors);
+      }
+    }})
+  */
     executionFunction(this.onResolve, this.onReject);
   }
 
+  // adds function to secrets chain
   then(onResolve) {
     this.secretsChain.push(onResolve);
-
+    //returns this so that another then or catch may be called
     return this;
   }
 
@@ -22,17 +36,20 @@ class Secret {
   }
 
   onResolve(value){
-    this.storedValue = value;
+    let storedValue = value;
 
     try {
       this.secretsChain.forEach((nextFunc) => {
-        this.storedValue = nextFunc(storedValue);
+        storedValue = nextFunc(storedValue);
       });
     } catch (error) {
       this.secretsChain = [];
 
       this.onReject(error);
     }
+  }
 
+  onReject(error) {
+    this.handleError(error);
   }
 }
